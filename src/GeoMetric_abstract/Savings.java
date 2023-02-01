83653bcc9c76857bbf9a73991f109c7732a3619c
Rajiv <ramac@rajivs-mbp.myfiosgateway.com>
2023-02-01 01:16:33 -0500

    first push

---
+++ src/GeoMetric_abstract/Savings.java
@@ -0,0 +1,44 @@
+package GeoMetric_abstract;
+
+public class Savings extends GeometricObject {
+    private double width;
+    private double height;
+
+    public Savings() {
+    }
+
+    public Savings(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    /** Return width */
+    public double getWidth() {
+        return width;
+    }
+
+    /** Set a new width */
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    /** Return height */
+    public double getHeight() {
+        return height;
+    }
+
+    /** Set a new height */
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override /** Return area */
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override /** Return perimeter */
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+}
